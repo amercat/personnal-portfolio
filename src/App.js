ddc992292ac3940a8ac7157580ed10748528b3dc
amercat25 <tbaptiste25@outlook.com>
2024-03-01 13:17:31 +0100

    edit logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from './components/Projects';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <NavBar />
       <Banner />
-      </div>
+      <Skills />
+      <Projects />
 
 
+
+
+    </div>
   );
 }
 
